d32ed017455150dc6a0c4e25ceeeddc81c1de33f
konstantin.suftin@gmail.com <lab741593>
2022-08-15 22:24:11 +0300

    15/08/2022 Generating Numbers From Digits #1

--- CodeWars/Generating Numbers From Digits #1.py
+++ CodeWars/Generating Numbers From Digits #1.py
@@ -1,13 +1,18 @@
 from itertools import permutations as perm
 
+# def proc_arr(arr):
+#     answ = set()
+#     for i in perm(arr):
+#         temp = int("".join(i))
+#         answ.add(temp)
+#     return [len(answ), min(answ), max(answ)]
+
 def proc_arr(arr):
-    answ = set()
-    for i in perm(arr):
-        temp = int("".join(i))
-        answ.add(temp)
-        res = list(answ)
-    return [len(res), min(res), max(res)]
+    answ = set(int("".join(i)) for i in perm(arr))
+    return [len(answ), min(answ), max(answ)]
 
 a = ['1','2','2','3','2','3']
+b = ['8', '7', '6', '4', '2', '2', '1'] #[2520, 1224678, 8764221]
+c = ['1','2','3','0','5','1','1','3'] #[3360, 1112335, 53321110]
 
-print(proc_arr(a))
+print(proc_arr(c))
